e1e5172e5992b7bb1f331853f926dd8eaa407511
Torsten Oppermann <oppermann.torsten@googlemail.com>
2015-03-17 18:22:15 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE>
+<html>
+<head>
+</head>
+<body>
+    <form action="login.php">
+        <input type="text" />
+        <input type="password" />
+        <input type="submit" />
+    </form>
+</body>
+</html>
