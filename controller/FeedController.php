a19618b7a23bb28782beb2acebcab418b556c0c2
Torsten Oppermann <oppermann.torsten@googlemail.com>
2015-03-19 14:13:07 +0100

    cleaned feed controller

--- controller/FeedController.php
+++ controller/FeedController.php
@@ -3,32 +3,50 @@ require("AbstractController.php");
 require("./config/db.php");
 require("./classes/User.php");
 
+/**
+* Handles ajax requests regarding Feeds
+* 
+* @author Torsten Oppermann
+* @since 19.03.2015
+*/
 class FeedController extends AbstractController
 {
     private $user;
     private $dbConnection;
     
-    public function execute($request)
+    /**
+    * delegates request to the according internal method
+    * methods: delete, add
+    *
+    * @param $pRequest the ajax request data
+    */
+    public function execute($pRequest)
     {
         session_start();
         $this->user = $_SESSION["user"];
 
-        switch ($request["method"]) {
+        // determine the correct internal method
+        switch ($pRequest["method"]) {
             case "delete":
-                $feedId     = $request["feed"];
+                $feedId     = $pRequest["feed"];
                 $this->delete($feedId);
             break;
             case "add":
-                $feedName   = $request["feed_name"];
-                $feedUrl    = $request["feed_url"];
+                $feedName   = $pRequest["feed_name"];
+                $feedUrl    = $pRequest["feed_url"];
                 $this->add($feedName, $feedUrl);
             break;
             default: 
-                echo "DEFAULT";
+                echo "Wrong method supplied at FeedController";
             break;
         }
     }
     
+    /**
+    * delete a given feed and return the updated table
+    *
+    * @param $pFeedId
+    */
     private function delete($pFeedId)
     {
         // create database connection
@@ -48,17 +66,23 @@ class FeedController extends AbstractController
         exit();
     }
     
+    /**
+    * add a new feed and return the updated table
+    *
+    * @param $pName, $pUrl 
+    */
     private function add($pName, $pUrl)
     {
         // create database connection
         $this->dbConnection = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
             
         if (!$this->dbConnection->connect_errno) {
-            // insert new feed
+            // insert new feed safely
             $sql = $this->dbConnection->prepare("INSERT INTO jadu.feeds(display_name, url) VALUES(?, ?);") or die("a mysql error has occured: " . $this->dbConnection->errno);
             $sql->bind_param("ss", $pName, $pUrl);
             $sql->execute();
             
+            // update the xref table
             $this->dbConnection->query("INSERT INTO jadu.users_feeds(user_id, feed_id) VALUES(" . $this->user->getUserId() . ", LAST_INSERT_ID());") or die("a mysql error has occured: " . $this->dbConnection->errno); 
         } else {
             $this->errors[] = "Could not connect to Database " . DB_NAME . " at "  . DB_HOST ;
